87126084f18597bcc6f8df20a079d16e2a554b19
Pengfei Chen <pengfeichen.cn@gmail.com>
2019-06-25 11:52:25 +0800

    Fix flutter plugin dry run

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_stc_tutk_example/main.dart';
+import '../../example/lib/main.dart';
 
 void main() {
   testWidgets('Verify Platform version', (WidgetTester tester) async {
